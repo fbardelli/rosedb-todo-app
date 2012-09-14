e252a2eb41072fdb2c9059433a8716ce7ae3ef51
Frank Bardelli <frank.bardelli@gmail.com>
2012-09-14 16:20:11 -0400

    Got started on a view mode for tasks.  Added some data to tasks table.

--- todoapp/lib/todoapp.pm
+++ todoapp/lib/todoapp.pm
@@ -1,11 +1,16 @@
 package todoapp;
 use Dancer ':syntax';
-use Todo::DB::ORM::Task;
+use Todo::DB::ORM::Task::Manager;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+	template 'index';
+};
+
+get '/tasks' => sub {
+	my $tasks = Todo::DB::ORM::Task::Manager->get_tasks_iterator;
+	template 'tasks', { tasks => $tasks } ;
 };
 
 true;
