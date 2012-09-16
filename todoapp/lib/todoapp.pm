173e45c41d56bc270cf6b7bc1bca1e0805777639
frank <frank.bardelli@gmail.com>
2012-09-16 00:45:08 -0400

    Implemented add and delete actions for tasks

--- todoapp/lib/todoapp.pm
+++ todoapp/lib/todoapp.pm
@@ -1,6 +1,8 @@
 package todoapp;
 use Dancer ':syntax';
+use Todo::DB::ORM::Task;
 use Todo::DB::ORM::Task::Manager;
+use Todo::DB::ORM::StatusCode::Manager;
 
 our $VERSION = '0.1';
 
@@ -10,7 +12,25 @@ get '/' => sub {
 
 get '/tasks' => sub {
 	my $tasks = Todo::DB::ORM::Task::Manager->get_tasks_iterator;
-	template 'tasks', { tasks => $tasks } ;
+	my $status_codes = Todo::DB::ORM::StatusCode::Manager->get_status_code_iterator;
+	template 'tasks', { tasks => $tasks, status_codes => $status_codes };
+};
+
+post '/tasks' => sub {
+	my $task = Todo::DB::ORM::Task->new(
+		short_description  => params->{short_description},
+		long_description   => params->{long_description},
+		start_date         => params->{start_date},
+		end_date           => params->{end_date},
+		status             => params->{status}
+	);
+	$task->save;
+	return forward '/tasks', {}, { method => 'GET' };
+};
+
+del '/task/:task_id' => sub {
+	my $task = Todo::DB::ORM::Task->new( id => params->{task_id} );
+	$task->delete;
 };
 
 true;
