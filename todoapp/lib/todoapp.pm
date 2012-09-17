03ad1ed10428d303a4781869fd9c5b2881f94bee
Frank Bardelli <frank.bardelli@gmail.com>
2012-09-17 16:20:17 -0400

    tidied file to match company coding conventions

--- todoapp/lib/todoapp.pm
+++ todoapp/lib/todoapp.pm
@@ -7,60 +7,61 @@ use Todo::DB::ORM::StatusCode::Manager;
 our $VERSION = '0.1';
 
 get '/tasks' => sub {
-    my $tasks = Todo::DB::ORM::Task::Manager->get_tasks_iterator(
-        sort_by => params->{sort_field} || 'id',
-        (
-            params->{filter}
-            ? (
-                query => [
-                    or => [
-                        short_description => { like => '%' . params->{filter} . '%' },
-                        long_description  => { like => '%' . params->{filter} . '%' },
-                    ]
-                ]
-              )
-            : ()
-         )
-    );
-    my $status_codes = Todo::DB::ORM::StatusCode::Manager->get_status_code_iterator;
-    template 'tasks', { tasks => $tasks, status_codes => $status_codes, task_filter => params->{filter} };
+	my $tasks = Todo::DB::ORM::Task::Manager->get_tasks_iterator(
+		sort_by => params->{sort_field} || 'id',
+		(
+			params->{filter}
+			? (
+				query => [
+					or => [
+						short_description => { like => '%' . params->{filter} . '%' },
+						long_description  => { like => '%' . params->{filter} . '%' },
+					]
+				]
+			  )
+			: ()
+		)
+	);
+	my $status_codes = Todo::DB::ORM::StatusCode::Manager->get_status_code_iterator;
+	template 'tasks',
+	  { tasks => $tasks, status_codes => $status_codes, task_filter => params->{filter} };
 };
 
 post '/tasks' => sub {
-    my $task = Todo::DB::ORM::Task->new(
-        short_description => params->{short_description},
-        long_description  => params->{long_description},
-        start_date        => params->{start_date},
-        end_date          => params->{end_date},
-        status            => params->{status}
-    );
-    $task->save;
-    return forward '/tasks', {}, { method => 'GET' };
+	my $task = Todo::DB::ORM::Task->new(
+		short_description => params->{short_description},
+		long_description  => params->{long_description},
+		start_date        => params->{start_date},
+		end_date          => params->{end_date},
+		status            => params->{status}
+	);
+	$task->save;
+	return forward '/tasks', {}, { method => 'GET' };
 };
 
 del '/task/:task_id' => sub {
-    my $task = Todo::DB::ORM::Task->new( id => params->{task_id} );
-    $task->delete;
+	my $task = Todo::DB::ORM::Task->new( id => params->{task_id} );
+	$task->delete;
 };
 
 get '/task/:task_id' => sub {
-    my $task = Todo::DB::ORM::Task->new( id => params->{task_id} );
-    my $status_codes = Todo::DB::ORM::StatusCode::Manager->get_status_code_iterator;
-    $task->load;
-    template 'task', { task => $task, status_codes => $status_codes };
+	my $task = Todo::DB::ORM::Task->new( id => params->{task_id} );
+	my $status_codes = Todo::DB::ORM::StatusCode::Manager->get_status_code_iterator;
+	$task->load;
+	template 'task', { task => $task, status_codes => $status_codes };
 };
 
 put '/task/:task_id' => sub {
 	my $task = Todo::DB::ORM::Task->new(
-        id => params->{task_id},
-        short_description => params->{short_description},
-        long_description  => params->{long_description},
-        start_date        => params->{start_date},
-        end_date          => params->{end_date},
-        status            => params->{status}
-    );
+		id                => params->{task_id},
+		short_description => params->{short_description},
+		long_description  => params->{long_description},
+		start_date        => params->{start_date},
+		end_date          => params->{end_date},
+		status            => params->{status}
+	);
 	$task->load;
-    $task->save;
+	$task->save;
 };
 
 true;
